76e9bf4274a42dfa517d0d8d93c73d32b86a166a
joshua stewart <jks403@humboldt.edu>
2024-02-19 20:56:56 +0000

    committing unsaved changes

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.createServer(function(req,res) {
+
+	res.write("On the way to becoming a full stack engineer!");
+	res.end();
+
+}
+).listen(3000);
+
+console.log("Server started on port 3000");
+
+
