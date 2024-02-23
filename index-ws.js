e76ba57a21e5b3f9d1f56c45eb5b2e5e93be48c0
Joshua K. Stewart <jks403@humboldt.edu>
2024-02-22 17:59:58 -0800

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -6,8 +6,22 @@ app.get('/', function(req,res) {
     res.sendFile('index.html', {root:__dirname});
 });
 
+
 server.on('request', app);
 server.listen(3000, function() {console.log('server started on port 3000');});
+
+
+
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    })
+})
+
 
 /** Begin Websocket  */
 
@@ -23,6 +37,11 @@ wss.on('connection', function connection(ws) {
     if(ws.readyState === ws.OPEN) {
         ws.send('Welcome to my server');
     }
+
+    db.run(`INSERT INTO visitors (count, time )
+        VALUES (${NumClients}, datetime('now'))
+        `);
+
 
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${NumClients}`);
@@ -41,6 +60,37 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
+}
+
+
+/** end WebSockets */
+/** begin database */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+    CREATE TABLE visitors (
+            count INTEGER, 
+            time TEXT
+    )
+    
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+
+
+function shutdownDB() {
+    getCounts();
+    console.log("shutting down db");
+    db.close();
 }
 
 
